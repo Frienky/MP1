897f20b73551db7147f39ef5e34170cf46cd32a6
Frienky <1171451551@qq.com>
2025-05-05 23:32:03 +0200

    clear page setting

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,37 +3,16 @@ import { SimpleLayout } from '@/components/layout/SimpleLayout'
 
 export const metadata: Metadata = {
   title: 'Projects',
-  description: projectHeadLine,
+  description: 'Projects page',
 }
 
 export default function Projects() {
   return (
     <SimpleLayout
-      title={projectHeadLine}
-      intro={projectIntro}
+      title="Projects"
+      intro="This is the projects page."
     >
-      <ul
-        role="list"
-        className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 pb-10"
-      >
-        {projects.map((project) => (
-          <ProjectCard key={project.name} project={project} />
-        ))}
-      </ul>
-      <div className="mx-auto flex flex-col max-w-xl gap-6 lg:max-w-none my-4 py-8 border-t border-muted">
-          <h2 className="flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight md:text-3xl opacity-80 mb-4">
-            <Calendar size={28}/>
-            Hobbies & Volunteer
-          </h2>
-          <ul
-            role="list"
-            className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
-          >
-            {activities.map((activity) => (
-              <ActivityCard key={activity.name} activity={activity} titleAs='h3'/>
-            ))}
-          </ul>
-        </div>
+      <p>No projects to display.</p>
     </SimpleLayout>
   )
 }
